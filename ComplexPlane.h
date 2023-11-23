e2b1238278a3b3ede1bddf3e36ab3cbb256640e5
Kevin Phan <kevin.theinventor>
2023-11-22 20:43:34 -0800

    first commit

---
+++ ComplexPlane.h
@@ -0,0 +1,15 @@
+#ifndef ComplexPlane_H
+#define ComplexPlane_H
+#include <iostream>
+
+using namespace std;
+const unsigned int MAX_ITER = 64;
+const float BASE_WIDTH = 4.0;
+const float BASE_HEIGHT = 4.0;
+const float BASE_ZOOM = 0.5;
+
+enum class State
+{
+	CALCULATING, DISPLAYING
+};
+#endif
